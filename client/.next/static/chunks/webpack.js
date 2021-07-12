08ff86f931ee116b58a2129a6a85dcb8bdf1fd3e
Boxuan Hu <hu.box@northeastern.edu>
2021-07-12 16:05:18 +0800

    server setup done

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "83ccecb4bd05e31d6955"; }
+/******/ 		__webpack_require__.h = function() { return "7c31d0da70557a49cce7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
